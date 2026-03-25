5a3cab31e30ad8afac2eb122953bc5c1f4e87bf7
malkist01 <164641045+malkist01@users.noreply.github.com>
2026-03-25 21:08:37 +0700

    Update manager_sign.h

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -25,6 +25,26 @@
 #define EXPECTED_SIZE_RESUKISU 0x377
 #define EXPECTED_HASH_RESUKISU                                                 \
     "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"
+    
+// Wild/WildKSU
+#define EXPECTED_SIZE_WILD 0x381
+#define EXPECTED_HASH_WILD                                                 \
+    "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4"
+    
+// RapliX/MamboSU
+#define EXPECTED_SIZE_MAMBO 0x384
+#define EXPECTED_HASH_MAMBO                                                 \
+    "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"
+    
+// KernelSU-Next/KernelSU-Next
+#define EXPECTED_SIZE_NEXT 0x3e6
+#define EXPECTED_HASH_NEXT                                                 \
+    "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
+    
+// Three/Enjoy
+#define EXPECTED_SIZE_ENJOY 0x31c
+#define EXPECTED_HASH_ENJOY                                                 \
+    "1ab6077099505a4f5ff851732d5d965a4908af7f60c871f23b4b3a58e80e6cd3"
 
 // KOWX712/KernelSU
 #define EXPECTED_SIZE_KOWX712 0x375
